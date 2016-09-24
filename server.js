d55412d29fca2abfd2e58f8791cc85371e193932
Jordan Last <jordan.michael.last@gmail.com>
2016-09-24 02:11:29 -0600

    fixing paths, adding executable header

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const static = require('node-static');
 
 const options = {
-    key: fs.readFileSync('./localhost.key'),
-    cert: fs.readFileSync('./localhost.cert')
+    key: fs.readFileSync(`${__dirname}/localhost.key`),
+    cert: fs.readFileSync(`${__dirname}/localhost.cert`)
 };
 
 const file = new static.Server(process.cwd());
@@ -12,12 +14,11 @@ require('http2').createServer(options, (req, res) => {
     req.addListener('end', () => {
         file.serve(req, res, (error, result) => {
             if (error && error.status === 404) {
-                file.serveFile(`${process.cwd()}/index.html`, 200, {}, req, res);
+                file.serveFile(`/index.html`, 200, {}, req, res);
             }
         });
     }).resume();
 }).listen(8000, (error) => {
-
     if (error) {
         console.log(error);
     }
